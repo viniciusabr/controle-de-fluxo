cbf6f7d301adeee3c796f12ede042c22706e206d
Vinícius <viniciusabreu2001@gmail.com>
2024-05-06 15:22:07 -0300

    testando controle de fluxo

---
+++ controle-de-fluxo/src/SistemaEsolar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SistemaEsolar {
+    public static void main(String[] args) {
+        double nota1, nota2, media;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Por favor, insira a primeira nota do aluno:");
+            nota1 = scanner.nextDouble();
+
+            System.out.println("Por favor, insira a segunda nota do aluno:");
+            nota2 = scanner.nextDouble();        }
+
+        media = (nota1 + nota2)/2;
+
+        if (media == 7) {
+            System.out.println("Parabéns o aluno foi aprovado na média!");
+        }
+        if (media>7 && media<=10) {
+            System.out.println("Parabéns você foi aprovado com nota acima da média!");
+            
+        }
+        if (media<7) {
+            System.out.println("Infelizmente você ficou reprovado!");
+            
+        }
+        if (media>10) {
+            System.out.println("invalido!");
+        }
+
+
+    }
+}
