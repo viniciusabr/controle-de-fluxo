6c278783fc933c67f3daf2bab6c9db2e416921bb
Vinícius <viniciusabreu2001@gmail.com>
2024-05-06 22:48:22 -0300

    testando o for em arrays

---
+++ controle-de-fluxo/src/ExemploForDois.java
@@ -0,0 +1,9 @@
+public class ExemploForDois {
+    public static void main(String[] args) {
+        String alunos[] = {"Vinicius", "Marcos", "Joana", "Jonas", "Felipe", "sonia", "Zayla carolina", "Gabrielle"};
+
+        for(String aluno : alunos){
+            System.out.println("o nome do aluno é: " + aluno);
+        }
+    }
+}
