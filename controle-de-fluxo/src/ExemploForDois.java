dd7fe67da42e6555f0a048eb362f54e04f45c44b
Vinícius <viniciusabreu2001@gmail.com>
2024-05-11 12:03:43 -0300

    ajustando

--- controle-de-fluxo/src/ExemploForDois.java
+++ controle-de-fluxo/src/ExemploForDois.java
@@ -1,9 +1,14 @@
 public class ExemploForDois {
     public static void main(String[] args) {
-        String alunos[] = {"Vinicius", "Marcos", "Joana", "Jonas", "Felipe", "sonia", "Zayla carolina", "Gabrielle"};
 
-        for(String aluno : alunos){
-            System.out.println("o nome do aluno é: " + aluno);
+       String alunos[] = {"Vinicius", "Marcos", "Joana", "Jonas", "Felipe", "sonia", "Zayla carolina", "Gabrielle"};
+
+        for(int i=0; i<alunos.length; i++){
+            System.out.println("o nome do aluno na posição " + i + " é: " + alunos[i]);
+            
         }
+
+
+        
     }
 }
